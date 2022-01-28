c17323f1a2aeb7afbdf5a86916a99fed5ce764fa
Isaiah Chennault <94478099+Zeek9223@users.noreply.github.com>
2022-01-27 22:44:13 -0500

    Add files via upload

---
+++ CSC 4350 Software Engineering-CTW/hw3-ichennault1/hw3.py
@@ -0,0 +1,88 @@
+import math
+import string
+import cvs
+import os
+
+#1
+def function(str)
+	str=innput(“Enter a string”)
+	v=0
+	c=0
+	for i i str:
+    	if(i == ‘a’ or i == ‘e’ or i ==‘i’ or i == ‘o’ or i ==‘u’ or i == ‘A’ or i == ‘E’ or i ==‘I’ or i == ‘O’ or i ==‘U’
+         v = v+1;
+    else:
+         c =c +1
+		if( v < c)
+   			print(bol)
+		else if (v = c)
+    return equal
+
+#2
+def function(volume_of_cylinder)
+R = radius
+H = height
+volume_of_cylinder = (R*pi*H)**2
+
+#3
+def function(stringg)
+string=input(“Enter a string”)
+csvwriter = csv.writer(csvfile)
+strings = next(csvreader)
+results = “”
+	for x in strings
+   		reselts += x + “”
+	return results
+
+
+#4
+def function(list):
+	if(len(list) == 0):
+		return “”
+		result = “”
+	for x in lst:
+		result += x + “”
+		return result
+
+#5
+def function(reverse)
+string=input(“Enter a string”)
+csvwriter = csv.writer(reverse)
+	for line in reverse:
+	reverse.append(line)
+	return reverse
+
+#6
+def funnction(divison)
+a = innput()
+b = input()
+diviso = a/b
+except ZeroDivisionError:
+
+#7
+def function(samelist)
+listofint[]
+	for for n in list:
+		if  nnot in listofint:
+			lstof int.append(n)
+	return listofint
+
+#8
+def functionn(hw3_folder)
+try:
+os.mkdir(“hw3-folder”)
+expect OsError as error
+
+
+
+#Isaiah Chennault 002-60-4873
+
+
+
+
+
+
+
+       
+
+
